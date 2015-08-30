74c9aade6808a8646d9d7db15195de7d4ee5a44d
sdaniels <daniels.scott.r@gmail.com>
2015-08-30 14:27:51 -0400

    Site redesign

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,105 +1,29 @@
-(function ($, google) {
+(function () {
     'use strict';
 
     var site = {
         init: function () {
-            var options = {},
-                data = {
-                    labels: ['HTML', 'CSS', 'JavaScript', 'NodeJS', 'Python', 'Java', 'PHP', 'SQL', 'No-SQL', 'Useability', 'Software Design', 'Mobile'],
-                    datasets: [
-                        {
-                            label: 'My Skills',
-                            fillColor: 'rgba(151,187,205,0.5)',
-                            strokeColor: 'rgba(151,187,205,0.8)',
-                            pointColor: 'rgba(151,187,205,1)',
-                            pointStrokeColor: '#fff',
-                            pointHighlightFill: '#fff',
-                            pointHighlightStroke: 'rgba(151,187,205,1)',
-                            data: [90, 85, 92, 40, 45, 20, 60, 70, 30, 85, 85, 65]
-                        }
-                    ]
-                },
-                ctx = document.getElementById('chart').getContext('2d'),
-                myBarChart = new Chart(ctx).Radar(data, options),
-                theBody = document.getElementsByTagName('body');
+            document.addEventListener('click', this.handleClickEvents);
+        },
 
-            if (theBody.length) {
-                document.getElementsByTagName('body')[0].addEventListener('click', function (e) {
-                    var target = e.target,
-                        type = target.tagName.toLowerCase(),
-                        url;
+        handleClickEvents: function (e) {
 
-                    if (type === 'a') {
-                        e.preventDefault();
-                        url = target.getAttribute('href');
-                    }
 
-                    if (target.className.indexOf('e-nav') !== -1) {
-                        $('html, body').animate({
-                            scrollTop: $('#' + url.split('#')[1]).offset().top - 20
-                        }, 1200);
-                    }
+            var target = e.target;
+            var targetType =  target.tagName.toLowerCase();
 
-                    if (target.className.indexOf('ext-link') !== -1) {
-                        window.open(url, '_blank');
-                    }
-                });
+            if (targetType === 'i') {
+                target = target.parentNode;
             }
 
-            google.maps.event.addDomListener(window, 'load', site.mapInit);
-        },
-        mapInit: function () {
-            var myLatlng = new google.maps.LatLng(42.241624, -83.588372),
-                mapCanvas = document.getElementById('map'),
-                mapOptions = {
-                    center: myLatlng,
-                    zoom: 5,
-                    disableDefaultUI: true,
-                    scrollwheel: false,
-                    draggable: false,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    styles: [{
-                        featureType: 'landscape',
-                        stylers: [{saturation: -100}, {lightness: 65}, {visibility: 'on'}]
-                    }, {
-                        featureType: 'poi',
-                        stylers: [{saturation: -100}, {lightness: 51}, {visibility: 'simplified'}]
-                    }, {
-                        featureType: 'road.highway',
-                        stylers: [{saturation: -100}, {visibility: 'simplified'}]
-                    }, {
-                        featureType: 'road.arterial',
-                        stylers: [{saturation: -100}, {lightness: 30}, {visibility: 'on'}]
-                    }, {
-                        featureType: 'road.local',
-                        stylers: [{saturation: -100}, {lightness: 40}, {visibility: 'on'}]
-                    }, {
-                        featureType: 'transit',
-                        stylers: [{saturation: -100}, {visibility: 'simplified'}]
-                    }, {
-                        featureType: 'administrative.province',
-                        stylers: [{visibility: 'off'}]
-                    }, {
-                        featureType: 'administrative.locality',
-                        stylers: [{visibility: 'off'}]
-                    }, {featureType: 'administrative.neighborhood', stylers: [{visibility: 'on'}]}, {
-                        featureType: 'water',
-                        elementType: 'labels',
-                        stylers: [{visibility: 'on'}, {lightness: -25}, {saturation: -100}]
-                    }, {
-                        featureType: 'water',
-                        elementType: 'geometry',
-                        stylers: [{hue: '#ffff00'}, {lightness: -25}, {saturation: -97}]
-                    }]
-                },
-                map = new google.maps.Map(mapCanvas, mapOptions),
-                marker = new google.maps.Marker({
-                    position: myLatlng,
-                    map: map
-                });
+            if (targetType === 'a' && target.className.indexOf('ext-link') !== -1) {
+                e.preventDefault();
+                window.open(target.href);
+            }
+
         }
     };
 
     site.init();
 
-})($, window.google);
+})();
--- assets/scripts/promises.js
+++ assets/scripts/promises.js
@@ -1,10 +1,15 @@
 
 // and…
-Promise.all([$.getJSON('http://hipsterjesus.com/api/'), $.getJSON('http://en.wikipedia.org/w/api.php?format=json&action=query&titles=India&prop=revisions&rvprop=content&callback=?')]).then(function(resp) {
+Promise.all([
+    $.getJSON('http://hipsterjesus.com/api/'),
+    $.getJSON('http://en.wikipedia.org/w/api.php?format=json&action=query&titles=India&prop=revisions&rvprop=content&callback=?')
+]).then(function (resp) {
+    'use strict';
     console.log(resp);
 
-}, function() {
-    console.log("something went wrong");
+}, function () {
+    'use strict';
+    console.log('something went wrong');
 });
 
 //http://en.wikipedia.org/w/api.php?format=json&action=query&titles=India&prop=revisions&rvprop=content&callback=?
